4d824a5f024782097bb732363f0aff418470f6ab
Max-Becker <maxbeckerr@gmail.com>
2017-10-05 19:56:09 -0700

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,12 @@
+package sample;
+
+import javafx.fxml.FXML;
+
+public class Controller {
+
+    @FXML
+    private void loginButtonHandler()
+    {
+        System.out.println("Log in button pressed.");
+    }
+}
